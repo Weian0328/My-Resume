ed3d7e7b915e97cd017b78ce2e2a09aa76fbd9eb
weian <kevin19960328@gmail.com>
2022-04-11 19:20:37 +0800

    create

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,7 +1,26 @@
 class ResumesController < ApplicationController
     def index
+        @resumes = Resume.all
     end
 
     def new
+        @resume = Resume.new
     end
+
+    def create
+        # render html: params
+        @resume = Resume.new(resume_params)
+
+        if @resume.save
+            # flash[:notice] = "新增成功"
+            redirect_to resumes_path, notice: "新增成功"
+        else
+            render :new
+        end
+    end
+
+    private
+      def resume_params
+        params.require(:resume).permit(:title, :content, :status)
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 # root "welcome#index"
-get "/" => "welcome#index"
-get"/resumes/new", to:"resumes#new", as: "new_resume"
+
+get "/" ,to:"welcome#index"
+get "/resumes/new", to:"resumes#new", as: "new_resume"
 get "/resumes", to:"resumes#index"
+post"/resumes", to:"resumes#create"
+post "/", to:"welcome#index" 
+
 end
